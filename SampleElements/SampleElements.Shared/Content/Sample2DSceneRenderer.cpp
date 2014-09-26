baf2f3efeadbf34889864d78264703e0a7e78d4c
nguoihocnghe <meodenlongtrang@gmail.com>
2014-09-26 18:32:21 +0700

    LoadResourceBitmap (Temp)

--- SampleElements/SampleElements.Shared/Content/Sample2DSceneRenderer.cpp
+++ SampleElements/SampleElements.Shared/Content/Sample2DSceneRenderer.cpp
@@ -1,7 +1,13 @@
+
 #include "pch.h"
 #include "Sample2DSceneRenderer.h"
 
 #include "Common/DirectXHelper.h"
+
+#ifndef HINST_THISCOMPONENT
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+#endif
 
 using namespace SampleElements;
 
@@ -37,26 +43,27 @@ void Sample2DSceneRenderer::Render()
 		);
 
 	context->BeginDraw();
+	context->Clear(D2D1::ColorF(D2D1::ColorF::CornflowerBlue));
 
-	context->DrawRectangle(
+	/*context->DrawRectangle(
 		D2D1::RectF(100.0f, 100.0f, 500.0f, 500.0f),
 		m_pBlueBrush.Get(),
 		4.0f,
 		m_stroke
 		);
 
-	context->FillRectangle(
+		context->FillRectangle(
 		D2D1::RectF(600.0f, 400.0f, 900.0f, 700.0f),
 		m_pLinearGradientBrush.Get()
 		);
 
-	context->DrawLine(
+		context->DrawLine(
 		D2D1::Point2F(200, 300),
 		D2D1::Point2F(800, 500),
 		m_pBlueBrush.Get(),
 		10.0f,
 		m_stroke
-		);
+		);*/
 
 	DX::ThrowIfFailed(
 		context->EndDraw()
@@ -91,14 +98,142 @@ void Sample2DSceneRenderer::CreateDeviceDependentResources()
 
 	DX::ThrowIfFailed(
 		context->CreateLinearGradientBrush(
-			D2D1::LinearGradientBrushProperties(D2D1::Point2F(600, 400), D2D1::Point2F(900, 700)),
-			pGradientStops,
-			&m_pLinearGradientBrush
-			)
+		D2D1::LinearGradientBrushProperties(D2D1::Point2F(600, 400), D2D1::Point2F(900, 700)),
+		pGradientStops,
+		&m_pLinearGradientBrush
+		)
 		);
 }
 
 void Sample2DSceneRenderer::ReleaseDeviceDependentResources()
 {
 	m_pBlueBrush.Reset();
+}
+
+
+HRESULT Sample2DSceneRenderer::LoadResourceBitmap(
+	ID2D1RenderTarget *pRenderTarget, IWICImagingFactory *pIWICFactory, PCTSTR resourceName,
+	PCTSTR resourceType, UINT destinationWidth, UINT destinationHeight, ID2D1Bitmap **ppBitmap)
+{
+	IWICBitmapDecoder *pDecoder = NULL;
+	IWICBitmapFrameDecode *pSource = NULL;
+	IWICStream *pStream = NULL;
+	IWICFormatConverter *pConverter = NULL;
+	IWICBitmapScaler *pScaler = NULL;
+
+	HRSRC imageResHandle = NULL;
+	HGLOBAL imageResDataHandle = NULL;
+	void *pImageFile = NULL;
+	DWORD imageFileSize = 0;
+
+	// Locate the resource.
+	imageResHandle = FindResourceW(HINST_THISCOMPONENT, resourceName, resourceType);
+	HRESULT hr = imageResHandle ? S_OK : E_FAIL;
+	if (SUCCEEDED(hr))
+	{
+		// Load the resource.
+		imageResDataHandle = LoadResource(HINST_THISCOMPONENT, imageResHandle);
+
+		hr = imageResDataHandle ? S_OK : E_FAIL;
+	}
+
+
+
+	if (SUCCEEDED(hr))
+	{
+		// Lock it to get a system memory pointer.
+		pImageFile = LockResource(imageResDataHandle);
+
+		hr = pImageFile ? S_OK : E_FAIL;
+	}
+	if (SUCCEEDED(hr))
+	{
+		// Calculate the size.
+		imageFileSize = SizeofResource(HINST_THISCOMPONENT, imageResHandle);
+
+		hr = imageFileSize ? S_OK : E_FAIL;
+
+	}
+
+
+	if (SUCCEEDED(hr))
+	{
+		// Create a WIC stream to map onto the memory.
+		hr = pIWICFactory->CreateStream(&pStream);
+	}
+	if (SUCCEEDED(hr))
+	{
+		// Initialize the stream with the memory pointer and size.
+		hr = pStream->InitializeFromMemory(
+			reinterpret_cast<BYTE*>(pImageFile),
+			imageFileSize
+			);
+	}
+
+
+	if (SUCCEEDED(hr))
+	{
+		// Create a decoder for the stream.
+		hr = pIWICFactory->CreateDecoderFromStream(
+			pStream,
+			NULL,
+			WICDecodeMetadataCacheOnLoad,
+			&pDecoder
+			);
+	}
+
+
+	if (SUCCEEDED(hr))
+	{
+		// Create the initial frame.
+		hr = pDecoder->GetFrame(0, &pSource);
+	}
+
+
+	if (SUCCEEDED(hr))
+	{
+		// Convert the image format to 32bppPBGRA
+		// (DXGI_FORMAT_B8G8R8A8_UNORM + D2D1_ALPHA_MODE_PREMULTIPLIED).
+		hr = pIWICFactory->CreateFormatConverter(&pConverter);
+	}
+
+
+
+	if (SUCCEEDED(hr))
+	{
+
+
+
+		hr = pConverter->Initialize(
+			pSource,
+			GUID_WICPixelFormat32bppPBGRA,
+			WICBitmapDitherTypeNone,
+			NULL,
+			0.f,
+			WICBitmapPaletteTypeMedianCut
+			);
+
+
+
+	}
+
+
+	if (SUCCEEDED(hr))
+	{
+		//create a Direct2D bitmap from the WIC bitmap.
+		hr = pRenderTarget->CreateBitmapFromWicBitmap(
+			pConverter,
+			NULL,
+			ppBitmap
+			);
+
+	}
+
+	SafeRelease(&pDecoder);
+	SafeRelease(&pSource);
+	SafeRelease(&pStream);
+	SafeRelease(&pConverter);
+	SafeRelease(&pScaler);
+
+	return hr;
 }
--- SampleElements/SampleElements.Shared/Content/Sample2DSceneRenderer.h
+++ SampleElements/SampleElements.Shared/Content/Sample2DSceneRenderer.h
@@ -1,7 +1,22 @@
 #pragma once
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files:
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <wchar.h>
+#include <math.h>
+
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
 
 #include "..\Common\DeviceResources.h"
 #include "..\Common\StepTimer.h"
+
 namespace SampleElements
 {
 	// Render 2D elements
@@ -13,10 +28,16 @@ namespace SampleElements
 		void ReleaseDeviceDependentResources();
 		void Update(DX::StepTimer const& timer);
 		void Render();
+		HRESULT LoadResourceBitmap(
+			ID2D1RenderTarget *pRenderTarget, IWICImagingFactory *pIWICFactory, PCTSTR resourceName,
+			PCTSTR resourceType, UINT destinationWidth, UINT destinationHeight, ID2D1Bitmap **ppBitmap);
 	private:
 		// Cached pointer to device resources.
 		std::shared_ptr<DX::DeviceResources> m_deviceResources;
 		Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> m_pBlueBrush;
 		Microsoft::WRL::ComPtr<ID2D1LinearGradientBrush> m_pLinearGradientBrush;
+
+		Microsoft::WRL::ComPtr<ID2D1BitmapBrush> m_pBitmapBrush;
 	};
 }
+
